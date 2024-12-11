7b402a01ca71157955b87e37061996c73165c679
Kevyn Kelso <kevynkelso@outlook.com>
2024-12-11 08:17:49 -0700

    feat: install.sh

---
+++ install.sh
@@ -0,0 +1,4 @@
+sudo apt install libgirepository1.0-dev gcc libcairo2-dev pkg-config python3-dev gir1.2-gtk-3.0
+sudo apt install python3-gi python3-gi-cairo gir1.2-gtk-3.0
+sudo apt install build-essential libdbus-glib-1-dev libgirepository1.0-dev
+sudo apt-get install libdbus-1-dev libdbus-glib-1-dbus
